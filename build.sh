e75ca67e685538acd6d30ee47c9ee1e1aebef946
Andry Becerra <andryjbv@gmail.com>
2025-05-22 22:50:11 -0400

    Add Docker setup scripts

--- build.sh
+++ build.sh
@@ -9,14 +9,15 @@ set -e
 ###############################################
 # TODO: Install project dependencies if needed based on relevant config/lock files in the repo.
 # Note that we are developing the project, even if dependencies have been installed before, we need to install again to accommodate the changes we made.
-# <dependency-installation-commands>
+pip install --no-cache-dir --upgrade pip
+pip install --no-cache-dir -e ".[test]"
 # TODO: Configure project and environment variables
-# <config-commands>
+
 
 ###############################################
 # BUILD
 ###############################################
 echo "================= 0909 BUILD START 0909 ================="
 # TODO: Build the project if needed. Note that we are developing the project and making changes to it, even if it has been build before, we need to build it again.
-# <build-commands>
+python -m pip install -e .
 echo "================= 0909 BUILD END 0909 ================="
